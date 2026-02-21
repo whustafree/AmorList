ba1de3da98a98e7b5749025712d9a7ce915aabeb
whustafree <magustoregustavo@gmail.com>
2026-02-21 16:14:10 +0000

    s1

--- amorlist-vue/src/App.vue
+++ amorlist-vue/src/App.vue
@@ -1,30 +1,7 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="min-h-screen bg-gray-900 flex items-center justify-center">
+    <h1 class="text-4xl font-bold text-pink-500">
+      ¡AmorList en Vue está vivo! ❤️
+    </h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
